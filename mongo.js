c8341da19793f0a2dee297f12cc0de32bec2c879
Stefano Tudose <stfntudose@gmail.com>
2021-03-26 07:15:00 +0100

    1

--- mongo.js
+++ mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-require('dotenv').config();
-const { mongoPath } = (`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@senkodb.vhj2f.mongodb.net/test`)
+const dotenv = require("dotenv")
+dotenv.config();
+const mongoPath = (`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@senkodb.vhj2f.mongodb.net/test`)
 
 module.exports = async () => {
     await mongoose.connect(mongoPath)
