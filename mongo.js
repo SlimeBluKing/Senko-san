caa1066b7743f8c723e9c4dc7a25a13850fed94c
Stefano Tudose <stfntudose@gmail.com>
2021-03-26 11:17:22 +0100

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const dotenv = require("dotenv")
 dotenv.config();
-const mongoPath = (`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@senkodb.vhj2f.mongodb.net/Data`)
+const mongoPath = (`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@senkodb.vhj2f.mongodb.net/senkodata`)
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, { useNewUrlParser: true, useUnifiedTopology: true })
