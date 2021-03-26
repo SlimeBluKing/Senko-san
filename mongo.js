35a259bbf81db6fc7044c2ac21b15cf5dff2d94a
Stefano Tudose <stfntudose@gmail.com>
2021-03-26 07:20:37 +0100

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -4,9 +4,6 @@ dotenv.config();
 const mongoPath = (`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@senkodb.vhj2f.mongodb.net/test`)
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath, {
-        useNewUrlParser: true,
-        seUnifiedTopology: true,
-    })
+    await mongoose.connect(mongoPath, { useNewUrlParser: true, useUnifiedTopology: true })
     return mongoose
 }
